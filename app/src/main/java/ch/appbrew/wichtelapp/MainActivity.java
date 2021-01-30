4a7422250a2e3a8787cbdd229ed9223fbacfaf91
Largi <kuenzler.ma@gmail.com>
2021-01-30 14:08:36 +0100

    Implement Navigation Bar -Add Navigation Drawable Vector -Navigation graph -Drawer Menu -Add Layout fragment -Add Appbar and Header
    Start Code in MainActivity.java

--- app/src/main/java/ch/appbrew/wichtelapp/MainActivity.java
+++ app/src/main/java/ch/appbrew/wichtelapp/MainActivity.java
@@ -6,19 +6,33 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+    //Navigation Components:
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+    private NavController navController;
+
+    //AppBarConfiguration:
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
         FloatingActionButton fab = findViewById(R.id.fab);
