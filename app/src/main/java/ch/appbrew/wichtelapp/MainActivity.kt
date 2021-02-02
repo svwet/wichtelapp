b198d3c9bb454acde63fba870a3f25db88287d1f
Sven Wetter <sven.wetter@ajila.com>
2021-02-02 19:52:10 +0100

    Fix dependencies issues

--- app/src/main/java/ch/appbrew/wichtelapp/MainActivity.kt
+++ app/src/main/java/ch/appbrew/wichtelapp/MainActivity.kt
@@ -8,28 +8,24 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.*
 import androidx.navigation.ui.NavigationUI.navigateUp
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
 
-
-import androidx.navigation.ui.NavigationUI
-
-
-
 class MainActivity : AppCompatActivity() {
     //Navigation Components:
-    private lateinit var drawerLayout:DrawerLayout
-    private lateinit var navView:NavigationView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
     private lateinit var navController: NavController
 
 
     //AppBarConfiguration:
-    private val idSets = setOf(R.id.FirstFragment,R.id.fragment_gruppen,R.id.fragment_meineWunschliste, R.id.fragment_wichtelwunschliste, R.id.fragment_benutzereinstellung)
+    private val idSets = setOf(R.id.FirstFragment, R.id.fragment_gruppen, R.id.fragment_meineWunschliste, R.id.fragment_wichtelwunschliste, R.id.fragment_benutzereinstellung)
     private lateinit var appBarConfig: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,15 +44,16 @@ class MainActivity : AppCompatActivity() {
         //Workaround for: navController = findNavController(R.id.nav_host)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         navController = navHostFragment.navController
-        appBarConfig = AppBarConfiguration(idSets,drawerLayout)
+        appBarConfig = AppBarConfiguration(idSets, drawerLayout)
 
-        setupActionBarWithNavController(navController,appBarConfig)
+        setupActionBarWithNavController(navController, appBarConfig)
         navView.setupWithNavController(navController)
 
-        override fun onSupportNavigateUp(): Boolean {
-            val navController : NavController = navHostFragment.navController
-            return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
-        }
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController: NavController = findNavController(R.id.nav_host)
+        return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
     }
 }
