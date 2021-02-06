6c4ea461a720340611d539e4fdee81f25978382f
Sven Wetter <sven.wetter@swisscom.com>
2021-02-06 11:08:27 +0100

    Add database

--- app/src/main/java/ch/appbrew/wichtelapp/SignupActivity.java
+++ app/src/main/java/ch/appbrew/wichtelapp/SignupActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -14,10 +15,16 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -30,6 +37,7 @@ import java.util.regex.Pattern;
 public class SignupActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
+    private static final String TAG = "Message";
 
     /**
      * Creates a new signup instance.
@@ -85,6 +93,7 @@ public class SignupActivity extends AppCompatActivity {
                                     loadingProgressBar.setVisibility(View.INVISIBLE);
                                     return;
                                 } else {
+                                    addUserToDatabase(name, email);
                                     SignupActivity.this.startActivity(new Intent(SignupActivity.this, LoginActivity.class));
                                     SignupActivity.this.finish();
                                     Toast.makeText(getApplicationContext(), "New user registration", Toast.LENGTH_SHORT).show();
@@ -95,5 +104,27 @@ public class SignupActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void addUserToDatabase(String name, String user) {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        Map<String, Object> map = new HashMap<>();
+        map.put("Name", name);
+        map.put("Benutzer", user);
+
+        database.collection("Benutzer")
+                .add(map)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "Document added with ID " + documentReference.get());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error while adding document");
+                    }
+                });
     }
 }
