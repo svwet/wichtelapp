0f8139594c52f1bf09f63ee9e5b3e63d07d9ce19
Sven Wetter <sven.wetter@ajila.com>
2021-02-10 21:17:57 +0100

    Fix view null pointer

--- app/src/main/java/ch/appbrew/wichtelapp/MeineWunschlisteFragment.java
+++ app/src/main/java/ch/appbrew/wichtelapp/MeineWunschlisteFragment.java
@@ -3,10 +3,15 @@ package ch.appbrew.wichtelapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,18 +19,18 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MeineWunschlisteFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private ArrayList<MyWishListItem> myWishList;
+    private RecyclerView mRecyclerView;
+    private MyWishListAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private Button buttonInsert;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public MeineWunschlisteFragment() {
-        // Required empty public constructor
     }
 
     /**
@@ -36,13 +41,9 @@ public class MeineWunschlisteFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment MeineWunschlisteFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static MeineWunschlisteFragment newInstance(String param1, String param2) {
         MeineWunschlisteFragment fragment = new MeineWunschlisteFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -59,6 +60,58 @@ public class MeineWunschlisteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meine_wunschliste, container, false);
+        View view = inflater.inflate(R.layout.fragment_meine_wunschliste,
+                container, false);
+        createExampleList();
+        buildRecyclerView(view);
+        setButtons(view);
+        return view;
+    }
+    public void insertItem(int position) {
+        myWishList.add(position, new MyWishListItem(R.drawable.ic_user, "New Item At Position" + position, "This is Line 2"));
+        mAdapter.notifyItemInserted(position);
+    }
+    public void removeItem(int position) {
+        myWishList.remove(position);
+        mAdapter.notifyItemRemoved(position);
+    }
+    public void changeItem(int position, String text) {
+        myWishList.get(position).changeProductName(text);
+        mAdapter.notifyItemChanged(position);
+    }
+    public void createExampleList() {
+        myWishList = new ArrayList<>();
+        myWishList.add(new MyWishListItem(R.drawable.ic_user, "Line 1", "Line 2"));
+        myWishList.add(new MyWishListItem(R.drawable.ic_gruppe, "Line 3", "Line 4"));
+        myWishList.add(new MyWishListItem(R.drawable.ic_benutzereinstellung, "Line 5", "Line 6"));
+    }
+
+    public void buildRecyclerView(View view) {
+        mRecyclerView = view.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mAdapter = new MyWishListAdapter(myWishList);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        mAdapter.setOnItemClickListener(new MyWishListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                changeItem(position, "Clicked");
+            }
+            @Override
+            public void onDeleteClick(int position) {
+                removeItem(position);
+            }
+        });
+    }
+    public void setButtons(View view) {
+        buttonInsert = view.findViewById(R.id.button_insert);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = 0;
+                insertItem(position);
+            }
+        });
     }
 }
